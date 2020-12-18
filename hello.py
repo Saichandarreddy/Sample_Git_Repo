fed206537dffeb387055b4fea76dee84921ef0c4
saichandarreddyt@gmai.com <saichandarreddyt@gmail.com>
2020-12-18 17:52:38 +0530

    commit in master

--- hello.py
+++ hello.py
@@ -2,9 +2,6 @@ print("Welcome to Git Repo")
 print("hello, my first change in hello.py")
 print('Please make these changes')
 print("changes were made in github portal")
-# <<<<<<< HEAD
 
 def git_pull():
     print('repo was pulled and git_pull was added')
-# =======
-# >>>>>>> be7102050e305c2df926dd96d6751055a7244175
