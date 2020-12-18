2d35a52fbdf3d76abec11eab9c588934a434c3f8
saichandarreddyt@gmai.com <saichandarreddyt@gmail.com>
2020-12-18 18:51:25 +0530

    modifications done on patch1 branch

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ print("changes were made in github portal")
 
 def git_pull():
     print('repo was pulled and git_pull was added')
+
+def git_changes_from_patch_1():
+    print('trying to create merge conflicts')
